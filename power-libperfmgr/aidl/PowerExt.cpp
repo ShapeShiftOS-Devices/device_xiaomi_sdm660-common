5b56978789dd7ee0e3a190893eff08a92fdbea8b
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-01-30 13:11:35 +0800

    sdm660-common: power-libperfmgr: Adapt and rebrand for xiaomi_sdm660

--- power-libperfmgr/aidl/PowerExt.cpp
+++ power-libperfmgr/aidl/PowerExt.cpp
@@ -15,7 +15,7 @@
  */
 
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
-#define LOG_TAG "android.hardware.power-service.pixel.ext-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_sdm660.ext-libperfmgr"
 
 #include "PowerExt.h"
 
